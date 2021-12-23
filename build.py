9a86cf072d96ef0c926981616855da149f1b0aa0
Xiangdong Zeng <xdzeng96@gmail.com>
2021-12-24 03:32:42 +0800

    first commit

---
+++ build.py
@@ -0,0 +1,34 @@
+import json
+import random
+
+INPUT_FILE  = 'data.txt'
+OUTPUT_FILE = 'docs/data.js'
+CHAR_RANGE  = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/='
+
+def split(a, n):
+    k, m = divmod(len(a), n)
+    return [a[i*k+min(i, m):(i+1)*k+min(i+1, m)] for i in range(n)]
+
+with open(INPUT_FILE, 'r') as fp:
+    lines = [l[:-1] for l in fp.readlines() if l and l[0] != '#']
+
+lines = list(set(lines))
+random.shuffle(lines)
+data = split(lines, len(CHAR_RANGE))
+
+encryption = {c: i for i, c in zip(data, CHAR_RANGE)}
+decryption = {}
+
+for i, c in zip(data, CHAR_RANGE):
+    for s in i:
+        decryption[s] = c
+
+export_data = {
+    'encryption': encryption,
+    'decryption': decryption,
+}
+
+with open(OUTPUT_FILE, 'w') as fp:
+    json_str = json.dumps(export_data, ensure_ascii=False, separators=(',', ':'))
+    # json_str = json.dumps(export_data, ensure_ascii=False, indent=2)
+    fp.write(f'const data={json_str};')
